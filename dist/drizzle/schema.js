a4e1f143226ce0a729620ff2a133184d1894b2be
krishtina-khatiwada <krishtina.khatiwada@truenary.com>
2025-06-24 16:21:44 +0545

    fix:adding with gitignore file

--- dist/drizzle/schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Task = void 0;
-const mysql_core_1 = require("drizzle-orm/mysql-core");
-exports.Task = (0, mysql_core_1.mysqlTable)('Task', {
-    id: (0, mysql_core_1.serial)("id").primaryKey(),
-    taskname: (0, mysql_core_1.varchar)("task", { length: 255 }).notNull(),
-    status: (0, mysql_core_1.varchar)("status", { length: 15 }).notNull(),
-});
